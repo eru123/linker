1b8d6b01ee21c7a79d701314c16dd90f098eaebf
eru123 <yeoligoakino@gmail.com>
2020-12-16 23:38:59 +0800

    Rewrite

--- README.md
+++ README.md
@@ -3,11 +3,17 @@ PHP Framework for Building REST API
 ### Documentation
 Documentation coming soon.
 # Install 
+### Install via Linker installer (Recommended)
+```bash
+composer global require eru123/linker-installer
+linker create <project_name>
+cd <project_name>
+```
 ### Install Via Composer
+Do this if you know how core files works
 ```bash
 composer require eru123/linker
 ```
-
 ### Install For Contribution
 ```bash
 git clone https://github.com/eru123/linker.git
--- src/Application.php
+++ src/Application.php
@@ -3,107 +3,45 @@
 namespace Linker;
 
 class Application {
+    final public function __construct(array $paths){
 
-    public ?\Linker\Router $router = NULL;
-    public ?\Linker\PDO $pdo = NULL;
-    public \Linker\Request $request; 
-    public \Linker\Uploader $upload; 
+        $app = $linker = $this;
+
+        // Classes
+        $class = include_once(rtrim($paths["core"],"/")."/classes.php");
+
+        // Config
+        $config = [];
+        foreach($class["FS"]::scandirTree($paths["config"]) as $config_file)
+            $config[pathinfo($config_file)["filename"]] = include_once($config_file);
+
+        // PDO
+        if(isset($config["pdo"]) && @$config["pdo"]["use"] === TRUE)
+            $this->pdo = new $class["PDO"]($config["pdo"]);
+
+        // PDO Model
+        if(isset($this->pdo) && @$config["pdo"]["model"] === TRUE && count($config["pdo"]["models"] ?? []) > 0)
+            foreach($config["pdo"]["models"] as $model)
+                $this->model[$model] = new $class["PDOMODEL"]($model,$this->pdo);
+        if(count($this->model ?? []) > 0) $this->model = (object) $this->model;
+
+        // Upload 
+        if(isset($config["uploader"]) && @$config["uploader"]["upload"] === TRUE)
+            $this->upload = new $class["UPLOAD"]($config["uploader"]);
+        
+        // Download 
+        if(isset($config["uploader"]) && @$config["uploader"]["download"] === TRUE)
+            $this->download = new $class["DOWNLOAD"]();
 
 
-    private bool $use_env = false;
-    private bool $use_config = false;
-    private bool $use_router = false;
-    private bool $use_pdo = false;
-    private bool $use_uploader = false;
-    private bool $use_download = false;
-    private bool $use_parser = false;
-    private string $rootdir = './';
-    public ?object $config = NULL;
 
-    public function __construct(?string $rootdir = NULL){
-        $this->rootdir = $rootdir ?? $this->rootdir;
-        $this->request = new \Linker\Request;
-    }
-    public function loadEnv(): ?object {
-        if(!$this->use_config && !$this->use_env){
-            try {
-                $dotenv = \Dotenv\Dotenv::createImmutable($this->rootdir);
-                $dotenv->load();
-                $this->use_env = true;
-                $this->config = (object) $_ENV;
-            } catch (\Exception $E){
-                throw new \Exception("Invalid ENV File");
-            }
+
+        // Router
+        if(isset($config["router"]) && @$config["router"]["use"] === TRUE){
+            $route = (new $class["ROUTER"]($config["router"]))->result();
+            $this->route = (object) $route;
+            if(@$config["router"]["render"] == TRUE && is_file((string) $route["path"]))
+                include_once $route["path"];
         }
-        return $this->config;
-    }
-    public function loadConfig($config = 'config.php') : ?object {
-        if(!$this->use_env && !$this->use_config){
-            if(is_string($config) && file_exists($config)){
-                $file = include_once($config);
-                $this->config = (object) $file;
-            } elseif (is_array($config)){
-                $this->config = (object) $config;
-            } else throw new \Exception("Invalid Config");
-            $this->use_config = TRUE;
-        }
-        return $this->config;
-    }
-    public function loadAutoConfig(){
-        $possible_config_files = [
-            "config.php",
-            ".config.php",
-            "cfg.php",
-            ".cfg.php",
-            "cnfg.php",
-            ".cnfg.php",
-            "confg.php",
-            ".confg.php",
-            "env.php",
-            ".env.php"
-        ];
-        if(!$this->use_env && !$this->use_config && !$this->config){
-            try {
-                $this->loadEnv();
-            } catch (\Exception $E){
-                try {
-                    $found = FALSE;
-                    foreach($possible_config_files as $config){
-                        if(file_exists( rtrim($this->rootdir,'/').'/'.ltrim($config,'/')) && !$found){
-                            $found = TRUE;
-                            $this->loadConfig($config);
-                        }
-                    }
-                    if(!$found){
-                        throw new \Exception("Invalid config file. ");
-                    }
-                } catch(\Exception $E) {
-                    throw new \Exception("Both ENV file and config is invalid!");
-                }
-            }
-        }
-    }
-    public function useRouter(){
-        if(!$this->use_router){
-            $this->loadAutoConfig();
-            $this->router = new \Linker\Router($this);
-            $this->use_router = true;
-        }
-        return $this->router;
-    }
-    public function usePDO(){
-        if(!$this->use_pdo){
-            $this->loadAutoConfig();
-            $this->pdo = new \Linker\PDO($this);
-            $this->use_pdo = TRUE;
-        }
-        return $this->pdo;
-    }
-    public function include(string $file,bool $duplicate = false){
-        ($duplicate ? include($file) : include_once($file));
-    }
-    public function useUploader(array $config = []){
-        $this->upload = new \Linker\Uploader($config);
-        return $this->upload;
     }
 }
