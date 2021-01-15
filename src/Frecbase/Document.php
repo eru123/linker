3affb5f2937021a7ecdfe7f85683daa387c181d2
Jericho Aquino <yeoligoakino@gmail.com>
2021-01-15 16:25:01 +0800

    Added Frecbase Document - NoSQL

---
+++ src/Frecbase/Document.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Linker\Frecbase;
+
+use \Linker\Frecbase\Document\Core;
+
+class Document {
+    public $dir;
+    public $doc;
+    public $data = NULL;
+    private $core;
+    public function __construct(?string $dir = NULL,?string $doc= NULL){
+        $this->dir = $dir;
+        $this->doc = $doc;
+        $this->core = new Core($dir);
+    }
+    public function save(){
+        $this->core->setDir($this->dir);
+        return $this->core->set($this->doc,$this->data);
+    }
+    public function dir(string $dir){
+        $this->dir = $dir;
+        return $this;
+    }
+    public function directory(string $dir){
+        return $this->dir($dir);
+    }
+    public function doc(string $doc){
+        $this->doc = $doc;
+        return $this;
+    }
+    public function document(string $doc){
+        return $this->doc($doc);
+    }
+    public function getData($default = NULL){
+        $this->core->setDir($this->dir);
+        $this->data = $this->core->get($this->doc,$default);
+        return $this;
+    }
+    public function optimize(){
+        if($this->data == NULL){
+            $this->getData();
+        }
+        $this->core->set($this->doc,$this->data,TRUE);
+        return $this;
+    }
+    public function clear(){
+        $this->core->setDir($this->dir);
+        $this->core->set($this->doc,NULL,TRUE);
+        return $this;
+    }
+}
---
+++ src/Frecbase/Document/Core.php
@@ -0,0 +1,84 @@
+<?php 
+
+namespace Linker\Frecbase\Document;
+
+use \Linker\FileSystem\Core as FS;
+
+class Core {
+    private $dir;
+    public function __construct(?string $dir = NULL){
+        $this->dir = $this->setDir($dir);
+    }
+    public function setDir(string $dir){
+        $dir = rtrim($dir,"/");
+        $this->dir = $dir."/";
+        FS::fixDir($dir);
+    }
+    
+    public function getDocPath(string $docName){
+        return $this->dir.$docName.".php";
+    }
+    public function create(string $docName){
+        FS::fwrite($this->getDocPath($docName),"<?php \$data=NULL;\n");
+    }
+    private static function json_encode($arr)
+    {
+        $encoded = json_encode($arr);
+        $encoded = str_replace('\\\'', '\'', $encoded);
+        $encoded = str_replace('\'', '\\\'', $encoded);
+        return $encoded;
+    }
+    public function doCheck(string $doc){
+        $f = $this->getDocPath($doc);
+        if(!is_file($f)){
+            $this->create($doc);
+        }
+        return $f;
+    }
+    public function set(string $docName,$val,$overwrite = FALSE){
+        $f = $this->doCheck($docName);
+        switch (gettype($val)) {
+            case 'string':
+                $data = "\"$val\"";
+                break;
+            case 'object':
+                $nObj = self::json_encode($val);
+                $data = "json_decode('$nObj')";
+                break;
+            case 'array':
+                $nObj = self::json_encode($val);
+                $data = "json_decode('$nObj',true)";
+                break;
+            case 'boolean':
+                $data = $val ? "true" : "false";
+                break;
+            case 'NULL':
+                $data = "NULL";
+                break;
+            case 'null':
+                $data = "NULL";
+                break;
+            default:
+                $data = $val;
+                break;
+        }
+        return $overwrite ? 
+            FS::fwrite($f, "<?php\n\$data = $data;\n")  : 
+            FS::fappend($f, "\$data = $data;\n");
+    }
+    public function get(string $docName,$default){
+        $f = $this->doCheck($docName);
+        $get = function (string $file, $default) {
+            include $file;
+            return isset($data) ? $data : $default;
+        };
+        return $get($f, $default);
+    }
+    public function delete(string $docName){
+        $file = $this->getDocPath($docName);
+        if(is_file($file)){
+            return FS::del($file);
+        }
+        return FALSE;
+    }
+}
