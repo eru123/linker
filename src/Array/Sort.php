048b8de2b55a7c57351d999fad9964b1bb6ebcec
Jericho Aquino <yeoligoakino@gmail.com>
2021-01-15 18:05:30 +0800

    Added array sorting for multi dimensional array

---
+++ src/Array/Sort.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * can be use only in the specified multidimensional array format.
+ * array(
+ *     array(
+ *         "id" => 324,
+ *         "key" => "value"
+ *     ),
+ *     array(
+ *         "id" => 324,
+ *         "key" => "value"
+ *     )
+ * )
+ */
+namespace Linker\Array;
+
+class Sort {
+    
+}
